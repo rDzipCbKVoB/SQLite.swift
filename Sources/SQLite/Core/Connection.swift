rDzipCbKVoB/SQLite.swift

commit 4e6b1e00466299136f826ccb7f2c6995eb81b8b2

Import CSQLite by default, not just when using CocoaPods.

Sources/SQLite/Core/Connection.swift
@@ -28,7 +28,7 @@ import Dispatch
 import sqlite3
 #elseif SQLITE_SWIFT_SQLCIPHER
 import SQLCipher
-#elseif COCOAPODS
+#else
 import CSQLite
 #endif
 

Sources/SQLite/Core/Statement.swift
@@ -26,7 +26,7 @@
 import sqlite3
 #elseif SQLITE_SWIFT_SQLCIPHER
 import SQLCipher
-#elseif COCOAPODS
+#else
 import CSQLite
 #endif
 

Sources/SQLite/Helpers.swift
@@ -26,7 +26,7 @@
 import sqlite3
 #elseif SQLITE_SWIFT_SQLCIPHER
 import SQLCipher
-#elseif COCOAPODS
+#else
 import CSQLite
 #endif
 

commit 30d45eba9bb875d5841e437ff7b2b642868f195d

Fix regression introduced in 4e6b1e00466299136f826ccb7f2c6995eb81b8b2

Sources/SQLite/Core/Connection.swift
@@ -28,7 +28,7 @@ import Dispatch
 import sqlite3
 #elseif SQLITE_SWIFT_SQLCIPHER
 import SQLCipher
-#else
+#elseif SWIFT_PACKAGE || COCOAPODS
 import CSQLite
 #endif
 

Sources/SQLite/Core/Statement.swift
@@ -26,7 +26,7 @@
 import sqlite3
 #elseif SQLITE_SWIFT_SQLCIPHER
 import SQLCipher
-#else
+#elseif SWIFT_PACKAGE || COCOAPODS
 import CSQLite
 #endif
 

Sources/SQLite/Helpers.swift
@@ -26,7 +26,7 @@
 import sqlite3
 #elseif SQLITE_SWIFT_SQLCIPHER
 import SQLCipher
-#else
+#elseif SWIFT_PACKAGE || COCOAPODS
 import CSQLite
 #endif
 

Tests/SQLiteTests/ConnectionTests.swift
@@ -5,7 +5,7 @@ import XCTest
 import sqlite3
 #elseif SQLITE_SWIFT_SQLCIPHER
 import SQLCipher
-#else
+#elseif SWIFT_PACKAGE || COCOAPODS
 import CSQLite
 #endif
 